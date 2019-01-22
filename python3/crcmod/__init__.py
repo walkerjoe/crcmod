0594736738335e38eec6127c16ad9a35adb8a130
Joe Walker <walkerjoseph@google.com>
2019-01-22 15:02:35 -0800

    Remove unhelpful comment

--- python3/crcmod/__init__.py
+++ python3/crcmod/__init__.py
@@ -3,7 +3,6 @@ try:
     from crcmod.crcmod import *
     import crcmod.predefined
 except ImportError:
-    # Make this backward compatible
     from crcmod import *
     import predefined
 __doc__ = crcmod.__doc__
